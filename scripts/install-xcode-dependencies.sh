b489a1da234d76b628bd82585c818aa313a7bbe3
Sarah Purohit <winocm@chariot-requiem>
2019-11-17 20:14:44 -0800

    Use sudo here...
    Signed-off-by: Sarah Purohit <winocm@chariot-requiem>

--- scripts/install-xcode-dependencies.sh
+++ scripts/install-xcode-dependencies.sh
@@ -5,6 +5,5 @@ print_line "Setting up Xcode."
 
 # Set up Xcode.
 sudo xcode-select -switch /Applications/Xcode.app
-
-xcrun --kill-cache
-xcodebuild -runFirstLaunch
+sudo xcrun --kill-cache
+sudo xcodebuild -runFirstLaunch
