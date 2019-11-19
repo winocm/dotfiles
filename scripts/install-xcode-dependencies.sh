5fbb7578644a07a07c1f19af7749706be3c9fec6
Sarah Purohit <winocm@chariot-requiem>
2019-11-19 13:57:56 -0800

    Reorder Xcode install script and remove wine.
    Signed-off-by: Sarah Purohit <winocm@chariot-requiem>

--- scripts/install-xcode-dependencies.sh
+++ scripts/install-xcode-dependencies.sh
@@ -4,6 +4,6 @@ source functions/common
 print_line "Setting up Xcode."
 
 # Set up Xcode.
+sudo xcodebuild -runFirstLaunch
 sudo xcode-select -switch /Applications/Xcode.app
 sudo xcrun --kill-cache
-sudo xcodebuild -runFirstLaunch
